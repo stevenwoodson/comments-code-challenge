04126a84bb73a4b5c61ddcf5dba4c7e02160bf02
Steve Woodson <steve@beinclusive.app>
2022-06-16 23:21:27 -0500

    Initial commit, near base install of Express in /api/ and React in /client/

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# comments-code-challenge
+# Ghost Coding challenge
+
+This app is split into two root folders:
+
+- `/api` is the backend Express API
+- `/client` is the frontend React app
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './styles/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
