751cb038ab1217e3c47b56e22467042e0a801a34
Steve Woodson <steve@beinclusive.app>
2022-06-17 15:10:23 -0500

    Comment posting to API feature complete

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const { DateTime } = require("luxon");
 const router = express.Router();
 const connection = require("../db");
 
 /* GET comments listing. */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM comments', (error, results, fields) => {
+  connection.query('SELECT * FROM comments ORDER BY id DESC', (error, results, fields) => {
     if (error) throw error
     res.json(results);
   })
+});
+
+
+/* POST new comment. */
+router.post('/', function(req, res, next) {
+  connection.query({
+    sql: 'INSERT INTO comments (`name`, `text`, `posted`) VALUES (?, ?, ?)',
+    timeout: 5000, // 5s
+  },
+  [req.body.name ? req.body.name : 'anonymous', req.body.text, DateTime.now().toISO()],
+  function (error, results, fields) {
+    if (error) throw error;
+    res.json(results.insertId);
+  });
 });
 
 
@@ -15,7 +30,7 @@ router.get('/', function(req, res, next) {
 router.post('/:commentId/upvote', function(req, res, next) {
   connection.query({
     sql: 'SELECT * FROM comments WHERE id = ?',
-    timeout: 40000, // 40s
+    timeout: 5000, // 5s
   },
   [req.params.commentId],
   function (error, results, fields) {
@@ -25,7 +40,7 @@ router.post('/:commentId/upvote', function(req, res, next) {
     let upvotes = comment.upvotes + 1;
     connection.query({
       sql: 'UPDATE comments SET `upvotes` = ? WHERE id = ?',
-      timeout: 40000, // 40s
+      timeout: 5000, // 5s
     },
     [upvotes, req.params.commentId],
     function (error, results, fields) {
--- client/src/index.js
+++ client/src/index.js
@@ -15,6 +15,8 @@ root.render(
 /* ==========================================================================
    Ghost Code Challenge V1 - all native JavaScript
    ========================================================================== */
+const commenterOptions = ['Hannah', 'Kevin', 'Matt', 'Naz'];
+const commenter = commenterOptions[Math.floor(Math.random()*commenterOptions.length)];
 
 /**
  * Returns a relative calendar string based on the provided timestamp
@@ -30,6 +32,9 @@ const getFormattedDate = (timestamp) => {
   if (commentPosted === '1 hour ago') {
     commentPosted = DateTime.fromISO(timestamp).toRelativeCalendar({ unit: "minutes" });
   }
+  if (commentPosted === 'in 0 hours') {
+    commentPosted = 'just now';
+  }
 
   return commentPosted;
 }
@@ -43,7 +48,7 @@ const getFormattedDate = (timestamp) => {
 const getFormattedCommentHTML = (comment) => (`
   <li>
     <span class="avatar-container">
-      <img class="avatar" src="/images/${comment.name}.jpg" alt="">
+      <img class="avatar" src="/images/${( commenterOptions.indexOf(comment.name) > -1 ? `${comment.name}.jpg` : 'anonymous.png')}" alt="">
     </span>
     <div class="comment">
       <div class="meta">
@@ -58,12 +63,38 @@ const getFormattedCommentHTML = (comment) => (`
     </div>
   </li>`);
 
+
+const fetchComments = () => {
+  const commentsContainer = document.querySelector('main .comments');
+  fetch(process.env.REACT_APP_API_URL + "comments")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        // Clear the comments container first
+        commentsContainer.innerHTML = "";
+
+        // Gather the formatted HTML for each comment that was returned
+        result.forEach(comment => {
+          commentsContainer.insertAdjacentHTML('beforeend', getFormattedCommentHTML(comment));
+        });
+
+        // Add the upvote button click handlers
+        const upvoteLinks = commentsContainer.getElementsByClassName('link--upvote');
+        Array.from(upvoteLinks).forEach(link => {
+          link.addEventListener("click", onUpvote, false);
+        });
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+};
+
 /**
  * On Upvote click, trigger an API call
  *
  * @param {int} commentId
  */
-
  function onUpvote(e) {
   fetch(process.env.REACT_APP_API_URL + "comments/" + this.dataset.id + '/upvote', {method: 'POST'})
   .then(res => res.json())
@@ -79,30 +110,49 @@ const getFormattedCommentHTML = (comment) => (`
 
 
 /**
- * Temporarily fetching and displaying comments with Vanilla JS
+ * Set up comment form, randomize who is commenting every pageload
  */
-const commentsContainer = document.querySelector('main .comments');
-fetch(process.env.REACT_APP_API_URL + "comments")
+const commentForm = document.querySelector('main form.add-comment');
+commentForm.setAttribute("action", process.env.REACT_APP_API_URL + "comments/");
+const commenterInput = document.createElement("input");
+commenterInput.setAttribute("name", "name");
+commenterInput.setAttribute("type", "hidden");
+commenterInput.setAttribute("value", commenter);
+commentForm.appendChild(commenterInput);
+const commentAvatar = commentForm.querySelector('.avatar');
+commentAvatar.setAttribute("src", `/images/${commenter}.jpg`);
+
+// Submit the comment form and trigger a refresh when successful
+commentForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const formData = new FormData(commentForm)
+  let formDataJSON = {};
+  formData.forEach((value, key) => formDataJSON[key] = value);
+
+  fetch(process.env.REACT_APP_API_URL + "comments",{
+    method: "POST",
+    body: JSON.stringify(formDataJSON),
+    headers: new Headers({
+        'Content-Type': 'application/json'
+    })
+  })
   .then(res => res.json())
   .then(
     (result) => {
-      // Clear the comments container first
-      commentsContainer.innerHTML = "";
-
-      // Gather the formatted HTML for each comment that was returned
-      result.forEach(comment => {
-        commentsContainer.insertAdjacentHTML('beforeend', getFormattedCommentHTML(comment));
-      });
-
-      // Add the upvote button click handlers
-      const upvoteLinks = commentsContainer.getElementsByClassName('link--upvote');
-      Array.from(upvoteLinks).forEach(link => {
-        link.addEventListener("click", onUpvote, false);
-      });
+      const commentText = commentForm.querySelector('input[name=text]');
+      commentText.value = '';
+      fetchComments();
     },
     (error) => {
       console.error(error);
     }
   )
+}, true);
 
 
+/**
+ * Temporarily fetching and displaying comments with Vanilla JS
+ */
+
+fetchComments();
